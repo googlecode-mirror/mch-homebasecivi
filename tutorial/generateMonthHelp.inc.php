f95e7ab807c4145f80b07b3ab54ca94c8b1865c0
Brian Jacobel <bjacobel@gmail.com>
2013-12-07 14:33:20 -0500

    Help docs done

--- tutorial/generateMonthHelp.inc.php
+++ tutorial/generateMonthHelp.inc.php
@@ -1,87 +1,56 @@
 
-	<p> <strong>How to Generate and Publish Future Weeks on the Calendar (Managers Only)</strong>
+	<p> <strong>How to Generate and Publish Future months on the Calendar (Managers Only)</strong>
 	</p>
-	<p> To begin this activity, select <B>(manage weeks)</B> at the top of any calendar page, like this:<BR><BR>
-		<a href="tutorial/screenshots/managecal1.png" class="image" title="managecal1.png"
-			target="tutorial/screenshots/managecal1.png">
-		   <img src="tutorial/screenshots/managecal1.png" width="400px" border="1px"></a>
-	</p> <p>You should then see this page:
-		<a href="tutorial/screenshots/managecal2.png" class="image" title="managecal2.png"
-			target="tutorial/screenshots/managecal2.png">
-		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/managecal2.png" width="400px" border="1px" align="center"></a>
-		 <p>Each calendar week in this list has the status <i>archived</i>, <i>published</i>, or <i>unpublished</i>.
+	<p> To begin this activity, select <B>(manage months)</B> at the top of any calendar page, like this:<BR><BR>
+		   <img src="tutorial/screenshots/managecalendar3.png" width="400px" border="1px"></a>
+	</p> <p>You should then see this page:</p>
+		<img src="tutorial/screenshots/managecalendar4.png" width="400px" border="1px" align="center"></a>
+		 <p>Each calendar month in this list has the status <i>archived</i>, <i>published</i>, or <i>unpublished</i>.
 					 <ul>
-					 An <i>archived</i> week is any week that is fully in the past (as of the current date) -- its calendar cannot be changed.
-					 <br>A <i>published</i> week is is an active calendar week, visible and changeable by either managers or volunteers
+					 An <i>archived</i> month is any month that is fully in the past (as of the current date) -- its calendar cannot be changed.
+					 <br>A <i>published</i> month is is an active calendar month, visible and changeable by either managers or volunteers
 					 as schedule changes occur.
-					 <br>An <i>unpublished</i> week is a calendar week that is not visible to volunteers, and can only be changed or
+					 <br>An <i>unpublished</i> month is a calendar month that is not visible to volunteers, and can only be changed or
 					 published by a manager.
 					 </ul>
 					 <p>The <strong> View Archive/Hide Archive</strong> button in the lower right corner of this table allows you to
-					 either see or not see the archived weeks from the table.  (Most of the time you will not need to
-					 see the archived calendar weeks, so this button will simplify your view by showing only current and future scheduled weeks.)
+					 either see or not see the archived months from the table.  (Most of the time you will not need to
+					 see the archived calendar months, so this button will simplify your view by showing only current and future scheduled months.)
 	<p> From here, you can do any of the following:
 		<ul>
-			<li>Add a new week to the calendar from the master schedule</li>
-			<li>Publish an unpublished week</li>
-			<li>Unpublish a published week</li>
-			<li>Remove the newest unpublished calendar week from the database</li>
-			<li>Remove the oldest archived calendar week from the database
+			<li>Add a new month to the calendar from the master schedule</li>
+			<li>Publish an unpublished month</li>
+			<li>Unpublish a published month</li>
+			<li>Remove the newest unpublished calendar month from the database</li>
+			<li>Remove the oldest archived calendar month from the database
 		</ul>
-				<p>When you add a new week to the calendar, this will populate each day and shift with
-				volunteers from the master schedule. Before generating a new calendar week it is important to be sure the
+				<p>When you add a new month to the calendar, this will populate each day and shift with
+				volunteers from the master schedule. Before generating a new calendar month it is important to be sure the
 				master schedule is up to date.  So this tutorial and the tutorial <strong>How to Manage the Master Schedule</strong>
 				go hand-in-hand.
 
-				<p> <B>Step 0:</B> (<i>You will do this step only once.</i>)  When the system is first installed and you select <B>(manage weeks)</B>  on the
-				at the top of the calendar, you will see the following form:<p>
-					<a href="tutorial/screenshots/managecal3.png" class="image" title="managecal3.png" horizontalalign="center">
-					 &nbsp&nbsp&nbsp&nbsp <img src="tutorial/screenshots/managecal3.png" width="400px" border="1px" align="middle"></a>
-				<p> This form asks you to select the Month, Day, Year, Weekday Group, Weekend Group, and Family Room Group that will be used to
-				populate the <i>very first</i> week on your calendar.
-
-				<p>For example, the above selections show that you chose the week with September 28, 2008 to be the first scheduled
-				calendar week, and it should be populated with volunteers from Weekday Group 1, Weekend Group 1, and Family Room Group 1 from the Master Schedule.
-				When you hit <b>submit</b>, the first week of your calendar will be populated and the following 1-row table will appear:<p>
-				<a href="tutorial/screenshots/managecal4.png" class="image" title="manageCal4.png" >
-			    &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/manageCal4.png" width="400px" border="1px" align="middle"></a>
-
-				<p> <B>Step 1:</B> At any time in the future, you can use the <strong>Add new week</strong> button to generate and populate the next successive week on the calendar with volunteers from the Master Schedule.
-					<p><a href="tutorial/screenshots/managecal4.png" class="image" title="managecal4.png"
-	        			target="tutorial/screenshots/managecal4.png">
-			 		&nbsp&nbsp&nbsp&nbsp <img src="tutorial/screenshots/managecal4.png" width="400px" border="1px" align="bottom"></a>
-				<p> <B>Step 2:</B> So here is the result of adding the next calendar week following the week of September 29.<p>
-					<a href="tutorial/screenshots/managecal5.png" class="image" title="managecal5.png" horizontalalign="center"
-						target="tutorial/screenshots/managecal5.png">
-					 &nbsp&nbsp&nbsp&nbsp <img src="tutorial/screenshots/managecal5.png" width="400px" border="1px" align="middle"></a>
-				<p> &nbsp&nbsp&nbsp Notice that this new week is populated with a different Weekday/Weekend/Family Room group from the Master Schedule.
-				Since there are two weekday groups on the master schedule, each shift's volunteers in Group One (or Group Two) are scheduled every other week.
-				<br>Notice also that you may choose which group is scheduled before adding a new week.  Before you click the button, simply choose the weekday group, weekend group
-				and family room group that will populate this new week.
-					 <p>&nbsp&nbsp&nbsp Each week in this table is marked as <i>archived</i>, <i>published</i>, or <i>unpublished</i>:
+				<p> <B>Step 1:</B> At any time in the future, you can use the <strong>Add new month</strong> button to generate and populate the next successive month on the calendar with volunteers from the Master Schedule. This will automatically be done when you press "Next Month" on the calendar page.
+					<p><img src="tutorial/screenshots/managecalendar5.png" width="120px" border="1px" align="bottom"></a>
+				<p> <B>Step 2:</B> So here is the result of adding the next calendar month following the month of March 2014.<p>
+					<img src="tutorial/screenshots/managecalendar6.png" width="10%" border="1px" align="middle"></a>
+				<p> Notice that this new month is populated with a different monthday/monthend/Family Room group from the Master Schedule.
+				Since there are two monthday groups on the master schedule, each shift's volunteers in Group One (or Group Two) are scheduled every other month.
+				<br>Notice also that you may choose which group is scheduled before adding a new month.  Before you click the button, simply choose the monthday group, monthend group
+				and family room group that will populate this new month.
+					 <p>&nbsp&nbsp&nbsp Each month in this table is marked as <i>archived</i>, <i>published</i>, or <i>unpublished</i>:
 					 <ul>
-					 An <i>archived</i> week is any week that is fully in the past (as of the current date) -- its calendar cannot be changed.
-					 <br>A <i>published</i> week is is an active calendar week, visible and changeable by either managers or volunteers
+					 An <i>archived</i> month is any month that is fully in the past (as of the current date) -- its calendar cannot be changed.
+					 <br>A <i>published</i> month is is an active calendar month, visible and changeable by either managers or volunteers
 					 as schedule changes occur.
-					 <br>An <i>unpublished</i> week is a future calendar week that is not yet visible to volunteers, and can only be changed by
+					 <br>An <i>unpublished</i> month is a future calendar month that is not yet visible to volunteers, and can only be changed by
 					 a manager.
 					 </ul>
 					 <p>&nbsp&nbsp&nbsp The <strong> View Archive/Hide Archive</strong> button in the lower right corner of this table allows you to
-					 either see or remove the archived weeks from the table.
+					 either see or remove the archived months from the table.
 
-				<p> <B>Step 3:</B> You can now choose to <b>reset</b>, <b>view</b>, <b>edit</b>, <b>publish</b>, or <b>remove</b> this week
+				<p> <B>Step 3:</B> You can now choose to <b>view</b>, <b>publish</b>, or <b>remove</b> this month
 				from the calendar using the buttons on the right.<br>
-				 <br>&nbsp&nbsp&nbsp The <b>reset</b> button repopulates all the shifts in that week to their original settings from the master schedule.
-				 <br>&nbsp&nbsp&nbsp The <b>view</b> button takes you to the calendar where you can view all the shift assignments for that week.
-				 <br>&nbsp&nbsp&nbsp The <b>edit</b> button takes you to the calendar where you can edit individual shifts for that week.
-				 <br>&nbsp&nbsp&nbsp The <b>publish/unpublish</b> button controls whether the week is visible and editable by any volunteer who is working at the front desk.
-				 <br>&nbsp&nbsp&nbsp The <b>remove</b> button removes the most recent unpublished week from the calendar.  (This is rarely used, but will be
-				 handy when you want to regenerate and repopulate a new week using different groups from the Master Schedule.)
-				 <p> For example, here is what you see when you unpublish a published week on the calendar:<p>
-				 <a href="tutorial/screenshots/managecal6.png" class="image" title="managecal6.png"
-		        		target="tutorial/screenshots/managecal6.png">
-				 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/managecal6.png" width="400px" border="1px" align="middle"></a>
-				 <p>Hitting the <b>Back</b> button will return you to where you left off, like this:<p>
-				 <a href="tutorial/screenshots/managecal7.png" class="image" title="managecal7.png"
-		        		target="tutorial/screenshots/managecal7.png">
-				 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/managecal7.png" width="400px" border="1px" align="middle"></a>
+				 <br>&nbsp&nbsp&nbsp The <b>view</b> button takes you to the calendar where you can view all the shift assignments for that month.
+				 <br>&nbsp&nbsp&nbsp The <b>publish/unpublish</b> button controls whether the month is visible and editable by any volunteer who is working at the front desk.
+				 <br>&nbsp&nbsp&nbsp The <b>remove</b> button removes the most recent unpublished month from the calendar.  (This is rarely used, but will be
+				 handy when you want to regenerate and repopulate a new month using different groups from the Master Schedule.)
