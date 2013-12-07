50e0531c3fb987360325748cb7adedd7777dc486
Brian Jacobel <bjacobel@gmail.com>
2013-12-07 13:54:18 -0500

    Most of the help docs done

--- tutorial/manageCalendarHelp.inc.php
+++ tutorial/manageCalendarHelp.inc.php
@@ -1,25 +1,19 @@
 
 	<p> <strong>Working with the Calendar</strong>
-	<p>  The calendar has eight different pages, one for each venue: the <strong>Reception</strong> calendar, the <strong>Family room</strong> calendar, and six others.  
-	Each calendar shows all the persons scheduled to volunteer for each crew on every day of a single week.
-        <p>  <b>Step 1:</b>  To begin working with a calendar page, find <B>calendar:</B> and select a venue, like <B>Reception</B>:<BR><BR>
-		<a href="tutorial/screenshots/managecalendarstep1.png" class="image" title="managecalendarStep1.png"
-			horizontalalign="center" target="tutorial/screenshots/managecalendarstep1.png">
-		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/managecalendarstep1.png" width="10%" border="1px" align="center"></a>
-	<p>  <b>Step 2:</b>  You will then see the Reception volunteer calendar on the screen.  Here is that calendar for the week beginning March 14, 2011.
+	<p>  There are three calendars, one for each venue: the <strong>Foodbank</strong> calendar, the <strong>Pantry</strong> calendar, and the <strong>Soup Kitchen</strong>   
+	Each calendar shows all the persons scheduled to volunteer for each crew on every day of a single week. The calendar automatically shows you the relevant group based on your profile.
+        <p>  <b>Step 1:</b>  To begin working with a calendar page, click on "Calendar" in the main menu.
+
+	<p>  <b>Step 2:</b>  You will then see the calendar on the screen.  Here is an example calendar for the week beginning March 14, 2011.
 	Notice that <b>Vacancies</b> are highlighted for each crew that isn't full.'<BR><BR>
-		<a href="tutorial/screenshots/managecalendarstep2.png" class="image" title="managecalendarStep2.png"
-			horizontalalign="center" target="tutorial/screenshots/managecalendarstep2.png">
-		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/managecalendarstep2.png" width="10%" border="1px" align="center"></a>
+		<img src="tutorial/screenshots/managecalendar1.png" width="10%" border="1px" align="middle"></a>
+
 	<p> <B>Step 3:</B> You can view the calendar schedule for future and previous weeks by selecting the <strong>Next Week &gt&gt</strong>
 			 or <strong>&lt&lt Previous Week</strong> button on the top right or left corner of the calendar, like this:<BR><BR>
-		<a href="tutorial/screenshots/managecalendarstep3.png" class="image" title="managecalendarStep3.png"
-        	target="tutorial/screenshots/managecalendarstep3.png">
-		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/managecalendarstep3.png" width="10%" border="1px" align="middle"></a>
-	<p> <B>Step 4:</B> To make any calendar changes, you must first select <strong>(edit this week)</strong> at the top of the calendar, like this:<BR><BR>
-<a href="tutorial/screenshots/managecalendarstep4.png" class="image" title="managecalendarStep4.png"
-        	target="tutorial/screenshots/managecalendarstep4.png">
-		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/managecalendarstep4.png" width="10%" border="1px" align="middle"></a>
+		<img src="tutorial/screenshots/managecalendar2.png" width="10%" border="1px" align="middle"></a>
+
+	<p> <B>Step 4:</B> To make any calendar changes, you must first select <strong>(edit this month)</strong> at the top of the calendar, like this:<BR><BR>
+		<img src="tutorial/screenshots/managecalendar3.png" width="10%" border="1px" align="middle"></a>
 
 	<p> Managers make the following changes on any calendar:
 			<ul>
