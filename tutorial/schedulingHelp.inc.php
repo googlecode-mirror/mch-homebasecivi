6aabaca2674e8ab3201898121b165158f4323d36
allen <allen@bowdoin.edu>
2013-12-07 15:52:35 -0500

    changed [] notation to array() in viewSchedule.php and changed "shift" to "crew" in scheduling help page.

--- tutorial/schedulingHelp.inc.php
+++ tutorial/schedulingHelp.inc.php
@@ -14,16 +14,16 @@
 
 <p> The schedule is organized by weeks. Working down the calendar, each row corresponds to a week of the month. </p>
 
-<p>A "shift" is shown as a box associated with a particular day (e.g., Thursday) If a shift has vacancies, the number of vacancies is shown in <strong>boldface</strong>. </p>
+<p>A "crew" is shown as a box associated with a particular day (e.g., Thursday) If a crew has vacancies, the number of vacancies is shown in <strong>boldface</strong>. </p>
 
 <img src="tutorial/screenshots/vacancies.png" width="400px" border="1px" align="middle"></a>
 
-<p> <B>Step 2:</B> If you want to fill a vacancy for a shift, you must first select it. </p></br>
+<p> <B>Step 2:</B> If you want to fill a vacancy for a crew, you must first select it. </p></br>
 
-<p>For example, suppose you want to add a person to fill the vacancy on the <strong>Monday</strong> Week 1 crew.  To do this, you first select that shift on the schedule. </p></br>
+<p>For example, suppose you want to add a person to fill the vacancy on the <strong>Monday</strong> Week 1 crew.  To do this, you first select that crew on the schedule. </p></br>
 
 
-<p> <B>Step 3:</B> Now you will see a crew worksheet for the <strong>Monday</strong> Week 1 shift.</p></br>
+<p> <B>Step 3:</B> Now you will see a crew worksheet for the <strong>Monday</strong> Week 1 crew.</p></br>
 
 <img src="tutorial/screenshots/crewsheet.png" width="400px" border="1px" align="middle"></a>
 
@@ -42,11 +42,11 @@
 <img src="tutorial/screenshots/scheduleedited.png" width="400px" border="1px" align="middle"></a>
 
 
-<p> <B>Step 5:</B> Several additional <strong>Master Schedule</strong> functions are also available.  You can remove an entire crew, you can remove a person to create a vacancy, and add or remove a slot from the shift. </p>
+<p> <B>Step 5:</B> Several additional <strong>Master Schedule</strong> functions are also available.  You can remove an entire crew, you can remove a person to create a vacancy, and add or remove a slot from the crew. </p>
     <ul>
-	    <li>To remove an existing shift from the Master Schedule, select <strong>Remove Entire Crew</strong> on the worksheet shown in <B>Step 3</B>.</li>
-	    <li>To remove a person from a shift, select <strong>Remove Person/Create Vacancy</strong> on the worksheet shown in <B>Step 3</B>. </li>
-	    <li>To add a new slot to a shift, select <strong>Add Slot</strong> on the worksheet shown in <B>Step 3</B>. </li>
-	    <li>To remove a vacant slot from a shift, select <strong>Remove Vacant Slot</strong> on the worksheet shown in <B>Step 3</B>. </li>
+	    <li>To remove an existing crew from the Master Schedule, select <strong>Remove Entire Crew</strong> on the worksheet shown in <B>Step 3</B>.</li>
+	    <li>To remove a person from a crew, select <strong>Remove Person/Create Vacancy</strong> on the worksheet shown in <B>Step 3</B>. </li>
+	    <li>To add a new slot to a crew, select <strong>Add Slot</strong> on the worksheet shown in <B>Step 3</B>. </li>
+	    <li>To remove a vacant slot from a crew, select <strong>Remove Vacant Slot</strong> on the worksheet shown in <B>Step 3</B>. </li>
 	</ul>
 <p> <B>Step 6:</B> When you finish, you can return to any other function by selecting it on the navigation bar.</p>
