168811c34855a4876453747a39fcfaec857d0f2a
Brian Jacobel <bjacobel@gmail.com>
2013-12-07 14:31:02 -0500

    Add person help doc done

--- tutorial/addPersonHelp.inc.php
+++ tutorial/addPersonHelp.inc.php
@@ -1,33 +1,32 @@
+<style>img {margin-left:50px;}</style>
 
-	<p> <strong>How to Add People to the Database</strong>
-	<p> <B>Step 1:</B> On the navigation bar at the top of the page, find <B>people:</B> and select <B>add</B>, like this:<BR><BR>
-		<a href="tutorial/screenshots/addpersonstep1.png" class="image" title="addPersonStep1.png"
-			horizontalalign="center" target="tutorial/screenshots/addpersonstep1.png">
-		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/addpersonstep1.png" width="400px" border="1px" align="center"></a>
+<p> <strong>How to Add People to the Database</strong></p>
 
-	<p> <B>Step 2:</B> You should now see a long form with information like "First Name" and "Last Name" to be filled in.  That form begins like this:<BR><BR>
-        <a href="tutorial/screenshots/addpersonstep2.png" class="image" title="addPersonStep2.png"
-        	target="tutorial/screenshots/addpersonstep2.png">
-		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/addpersonstep2.png" width="400px" border="1px" align="middle"></a>
-	<BR>  Now you can fill in these fields with the correct information.
-						<br>(NOTE:&nbsp All fields marked by <font color="#FF0000">*</font>
-						are required before a person can be added to the database.) </p>
+<p> <B>Step 1:</B> On the navigation bar at the top of the page, find <B>people:</B> and select <B>add</B>, like this:</p></BR>
 
-	<p> <B>Step 3:</B> When you are finished, select the <B>Submit</B> button at the bottom of the page, like this:<BR><BR>
-		 <a href="tutorial/screenshots/addpersonstep3.png" class="image" title="addPersonStep3.png"
-		 	target="tutorial/screenshots/addpersonstep3.png">
-		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/addpersonstep3.png" width="400px" border="1px"  align="middle"></a>
+<img src="tutorial/screenshots/menubar.png" width="400px" border="1px" align="center">
 
-	<p> <B>Step 4:</B> If an error was made or you omitted a required field, <font color=#FF0000>red</font>
-	warnings will tell you what needs to be corrected, like this:<BR><BR>
-		<a href="tutorial/screenshots/addpersonstep4.png" class="image" title="addPersonStep4.png"
-		horizontalalign="center" target="tutorial/screenshots/addpersonstep4.png">
-		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/addpersonstep4.png" width="400px" border="1px" align="middle"></a>
+<p> <B>Step 2:</B> You should now see a long form with information like "First Name" and "Last Name" to be filled in.  That form begins like this:</p></br>
 
-	<br>After you have made these corrections, repeat <B>Step 3</B>.
+<img src="tutorial/screenshots/addperson.png" width="400px" border="1px" align="middle">
 
-	<p> <B>Step 5:</B> If you have no errors or omissions, you will see a page that looks like this:<BR><BR>
-		<a href="tutorial/screenshots/addpersonstep5.png" class="image" title="addPersonStep5.png"
-			horizontalalign="center" target="tutorial/screenshots/addpersonstep5.png">
-		&nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/addpersonstep5.png" width="400px" border="1px" align="middle"></a>
-		 <p> <B>Step 6:</B> When you finish, you can return to any other function by selecting it on the navigation bar.
+<p>Now you can fill in these fields with the correct information. </BR>
+
+(NOTE:&nbsp All fields marked by <font color="#FF0000">*</font> are required before a person can be added to the database.) </p>
+
+<p> <B>Step 3:</B> When you are finished, select the <B>Submit</B> button at the bottom of the page, like this:<BR><BR>
+
+<img src="tutorial/screenshots/saveperson.png" width="400px" border="1px"  align="middle">
+
+<p> <B>Step 4:</B> If an error was made or you omitted a required field, <font color=#FF0000>red</font>
+warnings will tell you what needs to be corrected, like this:</p></BR>
+
+<img src="tutorial/screenshots/savepersonerror.png" width="400px" border="1px" align="middle">
+
+<p>After you have made these corrections, repeat <B>Step 3</B>.</p>
+
+<p> <B>Step 5:</B> If you have no errors or omissions, you will see a page that looks like this:</p></br>
+	
+<img src="tutorial/screenshots/newpersonsuccess.png" width="400px" border="1px" align="middle"></a>
+
+<p> <B>Step 6:</B> When you finish, you can return to any other function by selecting it on the navigation bar.</p>
