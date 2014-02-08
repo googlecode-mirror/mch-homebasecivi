20c810aeb7e0de1e97fa8ef17febf64d17017a85
allen <allen@bowdoin.edu>
2014-02-08 16:33:13 -0500

    foxed issue #7

--- database/dbinfo.php
+++ database/dbinfo.php
@@ -9,9 +9,9 @@
 
 function connect() {
     $host = "localhost";
-    $database = "mchhomebasecivi";
-    $user = "mchhomebasecivi";
-    $pass = "foodyWr1!";
+    $database = "mchhomebasedb";
+    $user = "mchhomebasedb";
+    $pass = "cs315";
 
     $connected = mysql_connect($host, $user, $pass);
     if (!$connected)
